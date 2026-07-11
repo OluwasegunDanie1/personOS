2b1313d00ee46856f9833a76c532e79e4dac2d0d
Oluwasegun Daniel <dandev914@gmail.com>
2026-07-11 17:01:22 +0100

    refactor(mobile): establish application structure foundation

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,23 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'app/app.dart';
+
 void main() {
   runApp(const RelvioApp());
-}
-
-class RelvioApp extends StatelessWidget {
-  const RelvioApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(home: RelvioBootstrapScreen());
-  }
-}
-
-class RelvioBootstrapScreen extends StatelessWidget {
-  const RelvioBootstrapScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Relvio')));
-  }
 }
--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:relvio/main.dart';
+import 'package:relvio/app/app.dart';
 
 void main() {
   testWidgets('Relvio bootstrap screen displays the Relvio identifier', (
